d90befe7ea07a5af54675011a9deeb779d4253d4
Ermachkov Yaroslav <eryar.school@gmail.com>
2024-05-20 00:59:21 +0300

    fixed!

--- contest3/A.cpp
+++ contest3/A.cpp
@@ -4,49 +4,56 @@
 
 const int cMaxn = 1e5 + 100, cInf = 2009000999;
 
-std::vector<std::pair<int, int>> gr[cMaxn];
+struct Graph {
+ private:
+  int vertex;
+  int edges;
+  std::vector<std::pair<int, int>> gr[cMaxn];
 
-void Solve() {
-  int n;
-  int m;
-  std::cin >> n >> m;
-  for (int i = 0; i < m; ++i) {
-    int u;
-    int v;
-    int e;
-    std::cin >> u >> v >> e;
-    gr[u].push_back({v, e});
-    gr[v].push_back({u, e});
-  }
-  int kk;
-  std::cin >> kk;
-  std::vector<int> d(n, cInf);
-  d[kk] = 0;
-  std::set<std::pair<int, int>> st;
-  st.insert({0, kk});
-  while (!st.empty()) {
-    auto [x, y] = *st.begin();
-    st.erase(st.begin());
-    for (auto [u, e] : gr[y]) {
-      if (d[u] > (e + d[y])) {
-        st.erase({d[u], u});
-        d[u] = d[y] + e;
-        st.insert({d[u], u});
-      }
+ public:
+  void Init() {
+    std::cin >> vertex >> edges;
+    for (int i = 0; i < edges; ++i) {
+      int u;
+      int v;
+      int e;
+      std::cin >> u >> v >> e;
+      gr[u].push_back({v, e});
+      gr[v].push_back({u, e});
     }
   }
-  for (auto i : d) {
-    std::cout << i << ' ';
+
+  std::vector<int>& Diikstra(int start) {
+    std::vector<int> d(vertex, cInf);
+    d[start] = 0;
+    std::set<std::pair<int, int>> st;
+    st.insert({0, start});
+    while (!st.empty()) {
+      auto [x, y] = *st.begin();
+      st.erase(st.begin());
+      for (auto [u, e] : gr[y]) {
+        if (d[u] > (e + d[y])) {
+          st.erase({d[u], u});
+          d[u] = d[y] + e;
+          st.insert({d[u], u});
+        }
+      }
+    }
+    return d;
   }
-  for (int i = 0; i < n; ++i) {
-    gr[i].clear();
-  }
-}
+};
 
 int main() {
   int t = 1;
   std::cin >> t;
   while ((t--) != 0) {
-    Solve();
+    Graph tmp;
+    tmp.Init();
+    int start;
+    std::cin >> start;
+    auto distance = tmp.Diikstra(start);
+    for (auto i : distance) {
+      std::cout << i << ' ';
+    }
   }
 }
