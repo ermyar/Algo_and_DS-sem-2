b37a2e5a8699f04e687f3289ebbc01e6fb4ba25b
Ermachkov Yaroslav <eryar.school@gmail.com>
2024-05-20 00:19:56 +0300

    fixed

--- contest2/D.cpp
+++ contest2/D.cpp
@@ -4,62 +4,76 @@
 
 const int KMaxn = 1e6 + 100;
 
-std::vector<int> graph[KMaxn];
-std::vector<int> rgraph[KMaxn];
-std::vector<int> stst;
-int color[KMaxn];
-
-void Dfs(int vertex) {
-  color[vertex] = 1;
-  for (int uuuu : graph[vertex]) {
-    if (color[uuuu] == 1) {
-      continue;
-    }
-    Dfs(uuuu);
-  }
-  stst.push_back(vertex);
-}
-
-void Dfs1(int vertex, int cmp) {
-  color[vertex] = cmp;
-  for (int uuuu : rgraph[vertex]) {
-    if (color[uuuu] != 0) {
-      continue;
-    }
-    Dfs1(uuuu, cmp);
-  }
-}
-
-int main() {
+class Graph {
+ private:
   int number;
   int mumber;
-  std::cin >> number >> mumber;
-  for (int i = 0; i < mumber; ++i) {
-    int xxxx;
-    int yyyy;
-    std::cin >> xxxx >> yyyy;
-    graph[xxxx].push_back(yyyy);
-    rgraph[yyyy].push_back(xxxx);
+  std::vector<int> graph[KMaxn];
+  std::vector<int> rgraph[KMaxn];
+  std::vector<int> stst;
+  int color[KMaxn];
+
+  void Dfs(int vertex) {
+    color[vertex] = 1;
+    for (int uuuu : graph[vertex]) {
+      if (color[uuuu] == 1) {
+        continue;
+      }
+      Dfs(uuuu);
+    }
+    stst.push_back(vertex);
   }
-  for (int i = 1; i <= number; ++i) {
-    if (color[i] == 0) {
-      Dfs(i);
+
+  void Dfs1(int vertex, int cmp) {
+    color[vertex] = cmp;
+    for (int vertex_u : rgraph[vertex]) {
+      if (color[vertex_u] != 0) {
+        continue;
+      }
+      Dfs1(vertex_u, cmp);
     }
   }
-  for (int i = 1; i <= number; ++i) {
-    color[i] = 0;
-  }
-  std::reverse(stst.begin(), stst.end());
-  int cmp = 0;
-  for (auto iiii : stst) {
-    if (color[iiii] == 0) {
-      cmp++;
-      Dfs1(iiii, cmp);
+
+ public:
+
+  void Input() {
+    std::cin >> number >> mumber;
+    for (int i = 0; i < mumber; ++i) {
+      int xxxx;
+      int yyyy;
+      std::cin >> xxxx >> yyyy;
+      graph[xxxx].push_back(yyyy);
+      rgraph[yyyy].push_back(xxxx);
     }
   }
-  std::cout << cmp << '\n';
-  for (int i = 1; i <= number; ++i) {
-    std::cout << color[i] << ' ';
+
+  void Condens() {
+    for (int i = 1; i <= number; ++i) {
+      if (color[i] == 0) {
+        Dfs(i);
+      }
+    }
+    for (int i = 1; i <= number; ++i) {
+      color[i] = 0;
+    }
+    std::reverse(stst.begin(), stst.end());
+    int cmp = 0;
+    for (auto iiii : stst) {
+      if (color[iiii] == 0) {
+        cmp++;
+        Dfs1(iiii, cmp);
+      }
+    }
+    std::cout << cmp << '\n';
+    for (int i = 1; i <= number; ++i) {
+      std::cout << color[i] << ' ';
+    }
   }
+};
+
+int main() {
+  Graph tmp;
+  tmp.Input();
+  tmp.Condens();
   return 0;
 }
